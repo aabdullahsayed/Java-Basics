2d77e08a7917153ba822e534d7026ec96f74edc0
unknown <abdullahalsayed087@gmail.com>
2025-04-27 00:47:05 +0600

    Add LeapYearChecker.java

--- 02_Control_Flow/LeapYearChecker.java
+++ 02_Control_Flow/LeapYearChecker.java
@@ -1,1 +1,24 @@
- 
+import java.util.Scanner;
+
+public class LeapYearChecker{
+    
+    
+    static void check( int a){
+        
+        if((a%4 == 0)&&(a%100 != 0)||(a%400 == 0)){
+            System.out.print(a+" is a Leap year");            
+        }
+        
+        else {
+            System.out.print(a+" is not a Leap year");
+        }
+    }
+    public static void main(String[] args){
+        
+        Scanner uin = new Scanner(System.in);
+        int x = uin.nextInt();
+        
+        check(x);
+        
+    }
+}
