f95467763fb6776a24e48524aa0f91201f947039
unknown <abdullahalsayed087@gmail.com>
2025-05-05 22:49:29 +0600

    Add PositiveNegativeChecker.Java

--- 02_Control_Flow/PositiveNegativeChecker.java
+++ 02_Control_Flow/PositiveNegativeChecker.java
@@ -1,1 +1,29 @@
- 
+ import java.util.Scanner;
+
+public class PositiveNegativeChecker{
+    
+    static void check(int x){
+        
+        if(x>0)
+        {
+            System.out.print("Positive");
+}
+        else if (x==0){
+            System.out.print("Neutral number");
+        }
+        else {
+            System.out.print("Negative");
+        }
+        }
+            
+    
+    public static void main(String[] args){
+        
+        Scanner uin = new Scanner(System.in);
+        
+        int a = uin.nextInt();
+        
+        check(a);
+        
+    }
+}
