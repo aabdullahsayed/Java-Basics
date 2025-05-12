890fa2f73fa06d78efeb3840d47492400fe67160
unknown <abdullahalsayed087@gmail.com>
2025-05-12 07:16:15 +0600

    Add Random Code

--- 04_String_Operations/VowelConsonantCounter.java
+++ 04_String_Operations/VowelConsonantCounter.java
@@ -1,1 +1,30 @@
- 
+import java.util.Scanner;
+
+public class VowelConsonantCounter{
+    Scanner uin = new Scanner(System.in);
+    String a;
+    
+     void input(){
+       
+         System.out.println("Enter the String: ");
+       a = uin.nextLine();
+        
+        
+         uin.close();
+    }
+     
+     void display(){
+         
+         System.out.println("User Input of a string :"+a);
+     }
+     
+    
+   public static void main(String[] args){
+       
+       VowelConsonantCounter obj = new VowelConsonantCounter();
+      
+       obj.input();
+        obj.display();
+   }
+    
+}
