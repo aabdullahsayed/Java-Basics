7e75ea5342bbcdea8c5c3506b148036bd556c97c
unknown <abdullahalsayed087@gmail.com>
2025-08-20 09:57:38 +0600

    ADD CountDigits.java

--- 03_Mathematical/CountDigits.java
+++ 03_Mathematical/CountDigits.java
@@ -1,1 +1,27 @@
- 
+import java.util.Scanner;
+public class CountDigits{
+
+    static void digits(int a)
+    {
+
+        if(a == 0) System.out.print(1);
+  int count = 0;
+        while(a>0){
+
+            a = a/10;
+            count++;
+
+        }
+System.out.print("Number of digits :"+count);
+
+    }
+    public static void main(String[] args) throws Exception{
+
+        Scanner uin = new Scanner(System.in);
+
+        int n = uin.nextInt();
+
+        digits(n);
+
+    }
+}
