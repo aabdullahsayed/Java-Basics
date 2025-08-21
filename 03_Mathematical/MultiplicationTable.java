f8d250ecad285d3a0f62166bfd8d7b5b52ae67f1
unknown <abdullahalsayed087@gmail.com>
2025-08-21 23:58:32 +0600

    ADD new Source code .java file

--- 03_Mathematical/MultiplicationTable.java
+++ 03_Mathematical/MultiplicationTable.java
@@ -1,1 +1,20 @@
- 
+import java.util.Scanner;
+public class MultiplicationTable{
+    
+    static void table(int n){
+        
+        for(int i=1; i<=10;i++){
+            
+            System.out.print(n+" + "+i+" = "+i*n);
+        }
+    }
+    
+    public static void main(String[] args){
+        
+        Scanner uin = new Scanner(System.in);
+        
+        int a = uin.nextInt();
+        table(a);
+        
+    }
+}
